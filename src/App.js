e6b1e6e7b059e547103c6e6a40d7f1f8a027a2f5
thbob2 <33073278+thbob2@users.noreply.github.com>
2021-10-17 13:59:18 +0100

    updated dependencies

--- src/App.js
+++ src/App.js
@@ -2,12 +2,35 @@
 import './index.css';
 import Header from './components/Header';
 import Tasks from './components/Tasks';
+import { useState } from 'react';
 function App() {
-  
+  const[tasks,setTasks] = useState(
+    [
+        {
+            id:1,
+            text:'Be kind',
+            day: 'Dec 22th at 0:00am',
+            reminder: true,
+        },
+        {
+            id:2,
+            text:'be cool',
+            day: 'jun 22th at 0:00am',
+            reminder: true,
+        },
+        {
+            id:3,
+            text:'I am cool',
+            day: 'Dec 12th at 0:00am',
+            reminder: true,
+        }
+    ]
+)
+
   return (
     <div className="container">
       <Header /> 
-      <Tasks />
+      <Tasks tasks={tasks} />
     </div>
   );
 }
