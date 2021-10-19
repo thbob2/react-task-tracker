2c460c862af5b73f51f4b39a594e97731e6e0fbf
thbob2 <33073278+thbob2@users.noreply.github.com>
2021-10-19 20:12:27 +0100

    implemented addtask component

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import './index.css';
 import Header from './components/Header';
 import Tasks from './components/Tasks';
 import { useState } from 'react';
+import AddTask from './components/AddTask';
 function App() {
   const[tasks,setTasks] = useState(
     [
@@ -27,8 +28,21 @@ function App() {
     ]
 )
 
+/**
+ * ? add Task
+ */
+  
+  const addTask = (task) =>{
+    const id = Math.floor(Math.random()*10000) + 1
+    const newTask = {id,...task}
 
-// !Delete task 
+    setTasks([...tasks,newTask])
+  }
+
+
+/**  
+ * !Delete task 
+*/
   const deleteTask = (id) => {
       setTasks(tasks.filter((task) => task.id!==id))
   }
@@ -49,6 +63,7 @@ function App() {
   return (
     <div className="container">
       <Header /> 
+      <AddTask onAdd={addTask} />
       {
         tasks.length > 0 ? 
         (<Tasks tasks={tasks} 
