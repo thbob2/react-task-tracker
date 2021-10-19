62c2a726361a1c0392ff784717c71fd3fcc91e8d
thbob2 <33073278+thbob2@users.noreply.github.com>
2021-10-19 20:32:09 +0100

    added some fa components

--- src/App.js
+++ src/App.js
@@ -5,6 +5,8 @@ import Tasks from './components/Tasks';
 import { useState } from 'react';
 import AddTask from './components/AddTask';
 function App() {
+  const [showAddTask,setShowAddTask]= useState(false)
+
   const[tasks,setTasks] = useState(
     [
         {
@@ -62,8 +64,8 @@ function App() {
 
   return (
     <div className="container">
-      <Header /> 
-      <AddTask onAdd={addTask} />
+      <Header onAdd={() => setShowAddTask(!showAddTask)} showAdd={showAddTask} /> 
+      {showAddTask && <AddTask onAdd={addTask} />}
       {
         tasks.length > 0 ? 
         (<Tasks tasks={tasks} 
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,13 +1,16 @@
 import PropTypes from 'prop-types'
 import Button from './Button'
-const Header = ({title}) => {
-    const onClick =() => {
-        console.log('click')
-    }
+import { FaMarker } from 'react-icons/fa'
+import { FaRegTimesCircle } from 'react-icons/fa'
+const Header = ({title, onAdd, showAdd}) => {
+    
+    
     return (
         <header className='header'>
             <h1>{title}</h1>
-            <Button color='#2fb45a' text='Add' onClick={onClick} />
+            <Button 
+            color={showAdd ? '#e53238' : '#2fb45a'} 
+            text={showAdd ? <FaRegTimesCircle/> : <FaMarker />} onClick={onAdd} />
         </header>
     )
 }
