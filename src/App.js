1927315ecc6f070b7da287492b374a97d29fd248
thbob2 <33073278+thbob2@users.noreply.github.com>
2021-10-20 23:08:41 +0100

    implemented json-server function

--- src/App.js
+++ src/App.js
@@ -32,33 +32,72 @@ const fetchTasks = async () =>{
   return data
 }
 
+const fetchTask = async (id) =>{
+  const res = await fetch(`http://localhost:5000/tasks/${id}`,{
+    
+  })
+  const data = await res.json()
+
+  console.log(data)
+
+  return data
+}
 
 /**
  * ? add Task
  */
   
-  const addTask = (task) =>{
-    const id = Math.floor(Math.random()*10000) + 1
+  const addTask = async (task) =>{
+    
+    const res = await fetch('http://localhost:5000/tasks',{
+      method:'POST',
+      headers:{
+        'Content-type':'application/json'
+      },
+      body: JSON.stringify(task)
+    })
+    
+    const data = await res.json()
+    setTasks([...tasks,data])
+    /**const id = Math.floor(Math.random()*10000) + 1
     const newTask = {id,...task}
 
-    setTasks([...tasks,newTask])
+    setTasks([...tasks,newTask])*/
   }
 
 
 /**  
  * !Delete task 
 */
-  const deleteTask = (id) => {
-      setTasks(tasks.filter((task) => task.id!==id))
+  const deleteTask = async(id) => {
+    
+    await fetch(`http://localhost:5000/tasks/${id}`,{
+      method: 'DELETE',
+    })
+
+    setTasks(tasks.filter((task) => task.id!==id))
   }
 
 
 //* toggle reminder
 
-  const toggleReminder = (id) => {
+  const toggleReminder = async (id) => {
+
+    const taskToToggle = await fetchTask(id)
+    const updTask = {...taskToToggle,reminder:!taskToToggle.reminder}
+
+    const res = await fetch(`http://localhost:5000/tasks/${id}`,{
+      method: 'PUT',
+      headers: {
+        'Content-type': 'application/json'
+      },
+      body: JSON.stringify(updTask)
+    })
+
+    const data = await res.json()
     setTasks(tasks.map((task) => task.id === id 
     
-    ? { ...task, reminder: !task.reminder }
+    ? { ...task, reminder: data.reminder }
     : task
     )) 
 
